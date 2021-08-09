c09594a1bb63ef56bace7015a336725b8ec41348
Micael G. Reis <reis.micael@hotmail.com>
2021-08-09 02:37:53 +0100

    Update MyContract.sol
    import from github

--- MyContract.sol
+++ MyContract.sol
@@ -1,5 +1,8 @@
 pragma solidity 0.5.1;
 
+import "http://github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol"; 
+import "http://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
 contract MyContract {
     Person[] public people;
 
